6c99ee84af488f3a36b421a0837ced5c1bc1c1c5
Majid Ghasemi <mjghcr@gmail.com>
2020-12-04 17:24:04 +0330

    Initial commit to master

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const search = document.getElementById('search');
+const matchList = document.getElementById('match-list');
+
+// search through iran.json and fetch data
+const searchStates = async (searchText) => {
+  const response = await fetch('../data/iran.json');
+  const states = await response.json();
+
+  // get matches to current input
+
+  let matches = states.filter((state) => {
+    const regex = new RegExp(`^${searchText}`);
+    return state.province_name.match(regex) || state.county_name.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    matchList.innerHTML = '';
+  }
+
+  output(matches);
+};
+
+// Show results in html
+
+const output = (matches) => {
+  if (matches.length > 0) {
+    const markup = matches
+      .map(
+        (match) => `<div class="wrapper--result__result">
+            <h3 class="wrapper--result__state">${match.city_name}</h3>
+            <small>تاریخ شکل گیری شهر : ${match.city_creation_date_raw}</small>
+        </div>`
+      )
+      .join('');
+
+    matchList.innerHTML = markup;
+  }
+};
+
+search.addEventListener('input', () => searchStates(search.value));
